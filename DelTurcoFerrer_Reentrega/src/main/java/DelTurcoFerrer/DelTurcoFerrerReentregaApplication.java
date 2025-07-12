e5d45089a63e6805d4a4b97e96a99ea00963f747
milag <milag@Milagros>
2025-07-12 15:23:42 -0300

    Add update

---
+++ DelTurcoFerrer_Reentrega/src/main/java/DelTurcoFerrer/DelTurcoFerrerReentregaApplication.java
@@ -0,0 +1,12 @@
+package DelTurcoFerrer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DelTurcoFerrerReentregaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DelTurcoFerrerReentregaApplication.class, args);
+	}
+
+}
